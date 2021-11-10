2adf14514026cab369c8f218a411b905b00a5bcb
Daniel Timko <Timko002@cougars.csusm.edu>
2021-11-10 01:29:34 -0800

    blockchat second change

---
+++ contracts/BlockchatBlockchain.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.0;
+
+contract BlockchatBlockchain {
+	address[16] public blockchatters;
+	//adding a user
+	struct 
+	function addUser(address _address) public {
+
+	}
+	function getUsers() public view returns(address _addresses[]) {
+		return blockchatters;
+	}
+}
