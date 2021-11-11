1d598dce7e6711feddea5b0e5daa510c85e443a1
Daniel Timko <Timko002@cougars.csusm.edu>
2021-11-11 00:08:54 -0800

    blockchat seventh change

--- src/js/app.js
+++ src/js/app.js
@@ -3,23 +3,6 @@ App = {
   contracts: {},
 
   init: async function() {
-    /* Load pets.
-    $.getJSON('../pets.json', function(data) {
-      var petsRow = $('#petsRow');
-      var petTemplate = $('#petTemplate');
-
-      for (i = 0; i < data.length; i ++) {
-        petTemplate.find('.panel-title').text(data[i].name);
-        petTemplate.find('img').attr('src', data[i].picture);
-        petTemplate.find('.pet-breed').text(data[i].breed);
-        petTemplate.find('.pet-age').text(data[i].age);
-        petTemplate.find('.pet-location').text(data[i].location);
-        petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
-
-        petsRow.append(petTemplate.html());
-      }
-    });
-    */
     return await App.initWeb3();
   },
 
@@ -49,25 +32,12 @@ App = {
   },
 
   initContract: function() {
-    $.getJSON('BlockchatBlockchain.json', function(data) {
-      // Get the necessary contract artifact file and instantiate it with @truffle/contract
-      var AdoptionArtifact = data;
-      App.contracts.Adoption = TruffleContract(AdoptionArtifact);
-
-      // Set the provider for our contract
-      App.contracts.Adoption.setProvider(App.web3Provider);
-
-      // Use our contract to retrieve and mark the adopted pets
-      return App.markAdopted();
-    });
 
     return App.bindEvents();
   },
 
   bindEvents: function() {
-    /*
-    $(document).on('click', '.btn-adopt', App.handleAdopt);
-    */
+
   },
 
 
